93665ab4d2098d0fa38158de4705ae1cb05bf378
Cvidal84 <carlosvidalpuigcerver@gmail.com>
2025-07-31 17:29:08 +0200

    boostrap

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
+  //obtenemos los elementos del DOM
   const display = document.getElementById("display");
   const buttons = document.querySelectorAll("button[data-value]");
   const clearButton = document.getElementById("clear");
   const equalButton = document.getElementById("equal");
 
-  // Añadir número u operador al display
+  // Añadir el data-value del número u operador al final del display
   buttons.forEach((btn) => {
     btn.addEventListener("click", () => {
       display.value += btn.getAttribute("data-value");
     });
   });
 
-  // Borrar todo
+  // Borrar todo, deja el display value sin valor, en blanco
   clearButton.addEventListener("click", () => {
     display.value = "";
   });
